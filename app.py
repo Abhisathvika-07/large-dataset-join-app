0b2a805f90d03ec935f4d002dc36abfefa6fb1ef
Abhisathvika-07 <abhavanam707@gmail.com>
2026-03-01 00:07:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -232,7 +232,7 @@ if st.session_state.final_df is not None:
 # -------- VISUALIZATION --------
 st.subheader("📊 Visual Analytics")
 
-numeric_cols = final.select_dtypes(include=np.number).columns
+numeric_cols = final.select_dtypes(include=[np.number]).columns
 
 if len(numeric_cols) > 0:
 
@@ -290,4 +290,5 @@ if len(numeric_cols) > 0:
 
 else:
     st.info("Upload at least 2 related files to begin analysis.")
+
 
